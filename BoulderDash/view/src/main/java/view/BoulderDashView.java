ef253bba7cd8d6fbe77b46128f0116961431e0d3
OlivierDescoupsStudent <olivier.descoups@viacesi.fr>
2017-06-23 09:50:55 +0200

    Comment View

--- BoulderDash/view/src/main/java/view/BoulderDashView.java
+++ BoulderDash/view/src/main/java/view/BoulderDashView.java
@@ -1,35 +1,44 @@
 package view;
-//test
+
 
 import controller.BoulderDashController;
 import controller.PlayerController;
 import javax.swing.*;
 
+/**
+ * 
+ * Display all we need for the game
+ *
+ */
+
 public class BoulderDashView{
 
-
+/**
+ * display the number of diamond needed to end the level
+ * @return
+ */
 	public int displayDiamondNumber() {
-		// TODO - implement BoulderDashView.displayDiamondNumber
+		
 		throw new UnsupportedOperationException();
 	}
 	
 	/**
-	 * 
+	 * Display the sprite on the window
 	 * @param x
 	 * @param y
 	 * @param SPRITE
 	 */
 	public void displayMap(int x, int y, int SPRITE) {
-		// TODO - implement BoulderDashView.displayMap
+		
 		throw new UnsupportedOperationException();
 	}
 
 	/**
-	 * 
+	 * create the window
 	 * @param dimension
 	 */
 	public void displayWindow(int dimension) {
-		// TODO - implement BoulderDashView.displayWindow
+	
 		throw new UnsupportedOperationException();
 	}
 
