61cd7413a676d372a21da3f7e1f18aeedca47fe6
OlivierDescoupsStudent <olivier.descoups@viacesi.fr>
2017-06-23 16:35:02 +0200

    Key controller + comment

--- BoulderDash/view/src/main/java/view/keyController.java
+++ BoulderDash/view/src/main/java/view/keyController.java
@@ -1,34 +1,94 @@
 package view;
 
-import java.awt.event.*;
 
-public class keyController implements KeyListener {
+import java.awt.event.KeyEvent;
 
-	/**
-	 * 
-	 * @param keyEvent
-	 */
-	public void keyPressed(KeyEvent keyEvent) {
-		// TODO - implement keyController.keyPressed
-		throw new UnsupportedOperationException();
-	}
 
-	/**
-	 * 
-	 * @param keyEvent
-	 */
-	public void keyReleased(KeyEvent keyEvent) {
-		// TODO - implement keyController.keyReleased
-		throw new UnsupportedOperationException();
-	}
 
-	/**
-	 * 
-	 * @param keyEvent
-	 */
-	public void keyTyped(KeyEvent keyEvent) {
-		// TODO - implement keyController.keyTyped
-		throw new UnsupportedOperationException();
-	}
+/**
+ * 
+ * 
+ *
+ */
 
+public class keyController {
+
+    private int dx;
+    private int dy;
+    private int x;
+    private int y;
+
+
+    public keyController() {
+        
+        init();
+    }
+    
+    private void init() {
+        
+
+        x = 16;      
+        y = 16;        
+    }
+
+
+    public void move() {
+        x += dx;
+        y += dy;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+
+
+    public void keyPressed(KeyEvent e) {
+    	
+    	
+    	 
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            x = x -16;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            x = x + 16;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            y = y -16;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            y = y + 16;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = 0;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 0;
+        }
+    }
 }
